b1347b05e9f9384aff354b1439b2671b09ba3ce0
Hamdy-Mohamed <121273938+Hamdy-Mohamed@users.noreply.github.com>
2023-06-23 03:38:19 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 (function minify() {
   let code_input = document.querySelector(".code_input");
   let btn = document.querySelector(".btn");
-
   btn.onclick = () => {
     let input_value = code_input.value;
     let result = input_value.replace(/\s+/g, " ");
     code_input.value = result;
   };
-  code_input.addEventListener("focus", () => {
+
+  code_input.addEventListener("click", () => {
     code_input.style.transition = "0.4s";
     code_input.style.height = "7rem";
     code_input.style.width = "32rem";
     code_input.style.borderRadius = "1.1rem";
   });
+
   let facebook_icon = document.querySelector("#facebook");
   let github_icon = document.querySelector("#github");
   let linkedin_icon = document.querySelector("#linkedin");
